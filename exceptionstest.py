ed876fac8c2d643f78615c699264129e217707f8
Dawid Kozinski <dkozinsk@interia.pl>
2014-07-22 22:05:55 +0200

    Added test module related to exceptions

---
+++ exceptionstest.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from exceptions import BaseException
+
+def throws():
+	    raise RuntimeError('this is the error message')
+
+def fun(x):
+	print "---"
+	print x
+	print "---"
+	
+	if x == 5 or x == 8:
+		raise BaseException("Piatka lub Osemka")
+	try:   
+		y = fun(5)
+	except:
+		print x
+		print "1111111111"
+		try:
+
+			y = fun(8)
+		except:
+			print x
+			print "222222222"
+			raise
+                else:
+			val = 8
+			print val
+			return val
+		finally:
+			print "Clenup inner"
+
+	else:
+		val = x + 5
+		print val 
+		return val
+	finally:
+		print "Cleanup"
+
+def main():
+	print "++++++++++++++"
+	try:
+		fun(0)
+	except:
+		print "END"
+
+
+if __name__ == '__main__':
+	main()
+
